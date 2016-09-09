8f7a3674678f5d9037eba468f44e993753ceb9f6
Sergey Buryndin <buryndin@yandex.ru>
2016-09-09 16:20:47 +0700

    Added some icons to fontawesome4 library

--- src/iconlibs/fontawesome4.js
+++ src/iconlibs/fontawesome4.js
@@ -1,14 +1,17 @@
 JSONEditor.defaults.iconlibs.fontawesome4 = JSONEditor.AbstractIconLib.extend({
   mapping: {
-    collapse: 'caret-square-o-down',
-    expand: 'caret-square-o-right',
+    collapse: 'caret-down',
+    expand: 'caret-right',
     "delete": 'times',
     edit: 'pencil',
     add: 'plus',
     cancel: 'ban',
     save: 'save',
     moveup: 'arrow-up',
-    movedown: 'arrow-down'
+    movedown: 'arrow-down',
+    zoom_in: 'search-plus',
+    folder_open: 'folder-open-o',
+    upload: 'cloud-upload'
   },
   icon_prefix: 'fa fa-'
 });
